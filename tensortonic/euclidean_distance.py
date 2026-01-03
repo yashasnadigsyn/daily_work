44f8f4d3ad55378e6ac756a05b80f6460ed80f82
yashasnadigsyn <yashasnadigsyn@protonmail.com>
2026-01-03 18:06:38 +0530

    Initial

---
+++ tensortonic/euclidean_distance.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def euclidean_distance(x, y):
+    """
+    Compute the Euclidean (L2) distance between vectors x and y.
+    Must return a float.
+    """
+    x_arr, y_arr = np.asarray(x), np.asarray(y)
+    dist = np.sqrt(np.sum(np.square(x_arr - y_arr)))   
+    return dist
---
+++ tensortonic/sigmoid_numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    Vectorized sigmoid function.
+    """
+    X_arr = np.asarray(x, dtype=float)
+    sigmoid_x = (1) / (1 + np.exp(-X_arr))
+    return sigmoid_x
