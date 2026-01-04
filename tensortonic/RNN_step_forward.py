30619d4829fed3af0efa7d6935ce01b9cf7db99d
yashasnadigsyn <yashasnadigsyn@protonmail.com>
2026-01-04 16:46:33 +0530

    RNN and leaky Relu

---
+++ tensortonic/RNN_step_forward.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def rnn_step_forward(x_t, h_prev, Wx, Wh, b):
+    """
+    Returns: h_t of shape (H,)
+    """
+    # Write code here
+    x_t = np.asarray(x_t)
+    h_prev = np.asarray(h_prev)
+    Wx = np.asarray(Wx)
+    Wh = np.asarray(Wh)
+    b = np.asarray(b)
+    pre_act = (x_t @ Wx) + (h_prev @ Wh) + b
+    h_t = np.tanh(pre_act)
+    return h_t
+
---
+++ tensortonic/leaky_relu.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def leaky_relu(x, alpha=0.01):
+    """
+    Vectorized Leaky ReLU implementation.
+    """
+    x_arr = np.asarray(x)
+    out = np.where(x_arr>=0, x_arr, alpha*x_arr)
+    return out
